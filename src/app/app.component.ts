3d2dff18e8fe30a28993b85c92c6758b6f2a6193
Intern ACC <intern.acc@bgc-bd.com>
2024-01-21 12:35:11 +0600

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,BodyComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
